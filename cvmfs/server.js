92c2f6e978d5a0032b46eaf10d0e3e199e7fb7d8
Marcin Mokrzan <markar.mormar@gmail.com>
2019-09-27 11:41:33 +0200

    Added cvmfs-backend monitor repository for openshift

---
+++ cvmfs/server.js
@@ -0,0 +1,41 @@
+import { getJSONfromRpository } from './cvmfs';
+import express from 'express';
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+
+const app = express();
+// for parsing application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods","GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Controle");
+    next();
+});
+
+const port = process.env.OPENSHIFT_NODEJS_PORT
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP
+app.listen(port, () => `Server running on port ${port}`);
+
+app.listen(port, server_ip_address, function() {
+    console.log("Listening on " + server_ip_address + ", port " + port);
+})
+
+app.get(process.env.OPENSHIFT_NODEJS_API, async (req, res) => {
+    let repositoryName = req.query.name;
+    let repositoryWebsite = req.query.website
+
+    try {
+        console.log(`Fetching repository ${repositoryName} from ${repositoryWebsite}`);
+        const reposonseJSON = await getJSONfromRpository(repositoryWebsite, repositoryName);
+        // console.log("reposonseJSON",reposonseJSON)
+        res.json(reposonseJSON);
+    } catch (error) {
+        console.log(error)
+        res.status(404).send(error.message);
+    }
+});
